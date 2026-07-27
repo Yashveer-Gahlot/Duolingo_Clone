1665459ff79eaad3237c50bc73f91f66aa1a2b9c
Yashveer-Gahlot <yashgahlot660@gmail.com>
2026-07-27 13:40:58 +0530

    deleted

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Add backend folder to python path
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "/backend"))
-
-# pyrefly: ignore [missing-import]
-from app.main import app
